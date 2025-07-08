08ae19721cf9bb247e26ce3483ca6cc3e2ff6dfa
Regynald Zambrano <regynald.zambrano@gmail.com>
2025-07-08 11:01:13 -0500

    added user login in auth bloc

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:blog_app/features/auth/domain/entities/profile.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,23 +9,42 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
+  final UserLogin _userLogin;
 
-  AuthBloc({required UserSignUp userSignUp})
+  AuthBloc({required UserSignUp userSignUp, required UserLogin userLogin})
     : _userSignUp = userSignUp,
+      _userLogin = userLogin,
       super(AuthInitial()) {
-    on<AuthSignUpEvent>((event, emit) async {
-      emit(AuthLoading());
-      final res = await _userSignUp(
-        UserSignUpParams(
-          name: event.name,
-          email: event.email,
-          password: event.password,
-        ),
-      );
-      res.fold(
-        (failure) => emit(AuthFailure(failure.message)),
-        (profile) => emit(AuthSuccess(profile)),
-      );
-    });
+    on<AuthSignUpEvent>(_onAuthSignUpEvent);
+    on<AuthLoginEvent>(_onAuthLoginEvent);
+  }
+
+  void _onAuthSignUpEvent(
+    AuthSignUpEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    final res = await _userSignUp(
+      UserSignUpParams(
+        name: event.name,
+        email: event.email,
+        password: event.password,
+      ),
+    );
+    res.fold(
+      (failure) => emit(AuthFailure(failure.message)),
+      (profile) => emit(AuthSuccess(profile)),
+    );
+  }
+
+  void _onAuthLoginEvent(AuthLoginEvent event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final res = await _userLogin(
+      UserLoginParams(email: event.email, password: event.password),
+    );
+    res.fold(
+      (failure) => emit(AuthFailure(failure.message)),
+      (profile) => emit(AuthSuccess(profile)),
+    );
   }
 }
