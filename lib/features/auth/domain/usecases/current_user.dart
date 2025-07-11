0e4e389324ff9775bc695cb8ec230aa8eb7099c5
Regynald Zambrano <regynald.zambrano@gmail.com>
2025-07-11 11:36:33 -0500

    added use case current_user

---
+++ lib/features/auth/domain/usecases/current_user.dart
@@ -0,0 +1,14 @@
+import 'package:blog_app/core/errors/failures.dart';
+import 'package:blog_app/core/usecases/usecase.dart';
+import 'package:blog_app/features/auth/domain/entities/profile.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class CurrentUser implements UseCase<Profile, NoParams> {
+  final AuthRepository authRepository;
+  const CurrentUser(this.authRepository);
+  @override
+  Future<Either<Failure, Profile>> call(NoParams params) async {
+    return await authRepository.currentUser();
+  }
+}
