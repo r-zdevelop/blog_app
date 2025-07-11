029382efcec54ff30c6debc1460509485771d44e
Regynald Zambrano <regynald.zambrano@gmail.com>
2025-07-11 11:34:44 -0500

    added getCurrentUserData and currentUserSession

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -3,18 +3,43 @@ import 'package:blog_app/features/auth/data/models/profile_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthRemoteDataSource {
+  Session? get currenstUserSession;
   Future<ProfileModel> signUpWithEmailAndPassword(
     String name,
     String email,
     String password,
   );
   Future<ProfileModel> loginWithEmailAndPassword(String email, String password);
+  Future<ProfileModel?> getCurrentUserData();
 }
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   final SupabaseClient supabaseClient;
 
   AuthRemoteDataSourceImpl(this.supabaseClient);
+
+  @override
+  Session? get currenstUserSession => supabaseClient.auth.currentSession;
+
+  @override
+  Future<ProfileModel?> getCurrentUserData() async {
+    try {
+      if (currenstUserSession != null) {
+        final userData = await supabaseClient
+            .from('profiles')
+            .select()
+            .eq('id', currenstUserSession!.user.id)
+            .single();
+        return ProfileModel.fromJson(
+          userData,
+        ).copyWith(email: currenstUserSession!.user.email);
+      }
+
+      return null;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
 
   @override
   Future<ProfileModel> signUpWithEmailAndPassword(
