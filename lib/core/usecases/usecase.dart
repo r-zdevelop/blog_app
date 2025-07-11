438f8f3631059d55d920c490c078dee896134873
Regynald Zambrano <regynald.zambrano@gmail.com>
2025-07-11 11:34:02 -0500

    added no params class in use_case.dart

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -4,3 +4,8 @@ import 'package:fpdart/fpdart.dart';
 abstract interface class UseCase<SuccessType, Params> {
   Future<Either<Failure, SuccessType>> call(Params params);
 }
+
+class NoParams {
+  // This class can be used to pass no parameters to the use case.
+  // It is useful for cases where the use case does not require any input.
+}
